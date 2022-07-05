75d7907b8534c9b0128497f642afd493a5cb3365
Xu Gao <xugao0131@hotmail.com>
2022-07-05 08:02:39 -0700

    cleanup, package upgrades, TS support

---
+++ src/client/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import img from './react.png';
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <img src={img} alt="react img" />
+    </>
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const outputDirectory = 'dist';
 
 module.exports = {
-  entry: ['babel-polyfill', './src/client/index.js'],
+  entry: ['babel-polyfill', './src/client/index.tsx'],
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js'
   },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
+  },
+
   module: {
     rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader'
+      }]
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      use: [
+        {
+          loader: 'url-loader?limit=100000'
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
-      }
+      ]
+    },
+    // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+    { test: /\.tsx?$/, loader: 'ts-loader' },
+    // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+    { test: /\.js$/, loader: 'source-map-loader' }
     ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
   },
   devServer: {
     port: 3000,
@@ -40,7 +52,7 @@ module.exports = {
     }
   },
   plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico'
